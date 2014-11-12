13c7235217be8b43ae48aa476ca2a9a92da28f43
Max Maurer <maxemanuel.maurer@gmail.com>
2014-11-12 10:34:57 +0100

    now also with importing feature. export now creates correct data type index values and omits the index type at the end of the index

--- bin/riak-bucket-exporter.js
+++ bin/riak-bucket-exporter.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 var program = require('commander');
+var async = require('async');
 
 program
     .version('0.0.5')
@@ -8,6 +9,7 @@ program
     .option('-H, --host [host]','specify the host (default: localhost)')
     .option('-p, --port [port]','specify the post (default: 8098)')
     .option('-f, --file [FileName]','specify the file name (default: [bucket].json)')
+    .option('-i, --import','import mode (instead of reading from bucket entries will be written to bucket)')
     .parse(process.argv);
 if(!program.args.length) {
     program.help();
@@ -20,17 +22,51 @@ var count = 0;
 var openWrites = 0;
 var db = require("riak-js").getClient({host: program.host, port: program.port});
 var fs = require('fs');
-if (fs.existsSync(program.file)) {
-  throw new Error('the output file already exists');
+if (program.import) {
+  importToBucket();
+} else {
+  exportFromBucket();
 }
-console.log('fetching bucket '+bucket+' from '+program.host+':'+program.port);
-fs.appendFile(program.file, '[');
-db.keys(bucket,{keys:'stream'}, function (err) {
-  if (err) {
-    console.log('failed to fetch keys');
-    console.log(err);
+
+function importToBucket() {
+  if (!fs.existsSync(program.file)) {
+    throw new Error('the import file does not exist');
   }
-}).on('keys', handleKey).on('end', end).start();
+  fs.readFile(program.file, 'utf8', function (err,data) {
+    if (err) {
+      return console.log(err);
+    }
+    var entries = JSON.parse(data);
+    async.eachLimit(entries, 20, function(entry, cb) {
+      console.log('inserting entry with key %j', entry.key);
+      db.save(bucket, entry.key, entry.data, {index: entry.indexes}, function(err, res, receivedMetadata) {
+        if (err) {
+          return cb(err);
+        }
+        cb(null);
+      });
+    }, function(err) {
+      if (err) {
+        return console.log(err);
+      }
+      return console.log('%j entries inserted into bucket %j', entries.length, bucket);
+    });
+  });
+}
+
+function exportFromBucket() {
+  if (fs.existsSync(program.file)) {
+    throw new Error('the output file already exists');
+  }
+  console.log('fetching bucket '+bucket+' from '+program.host+':'+program.port);
+  fs.appendFile(program.file, '[');
+  db.keys(bucket,{keys:'stream'}, function (err) {
+    if (err) {
+      console.log('failed to fetch keys');
+      console.log(err);
+    }
+  }).on('keys', handleKey).on('end', end).start();
+}
 
 function end() {
   if (openWrites>0) {
@@ -75,11 +111,17 @@ function processKey(key) {
 
 function extractIndexes(meta) {
   var indexes = {};
-  var regex = /^x-riak-index-(.*)$/;
+  var regex = /^x-riak-index-(.*)_(.*)$/;
   for (var key in meta.headers) {
     var matches = key.match(regex);
     if (matches) {
-      indexes[matches[1]] = meta.headers[key];
+      var name = matches[1];
+      var type = matches[2];
+      var val = meta.headers[key];
+      if (type==='int') {
+        val = parseInt(val, 10);
+      }
+      indexes[name] = val;
     }
   }
   return indexes;
