fe24ded52a48d0cd1c0c1f1bf6c7c165a067ce5f
Max Maurer <maxemanuel.maurer@gmail.com>
2014-11-12 10:18:48 +0100

    now exporting to json and also including 2is

--- bin/riak-bucket-exporter.js
+++ bin/riak-bucket-exporter.js
@@ -3,11 +3,11 @@
 var program = require('commander');
 
 program
-    .version('0.0.1')
+    .version('0.0.5')
     .usage('[options] bucketName')
     .option('-H, --host [host]','specify the host (default: localhost)')
     .option('-p, --port [port]','specify the post (default: 8098)')
-    .option('-f, --file [FileName]','specify the file name (default: [bucket].txt)')
+    .option('-f, --file [FileName]','specify the file name (default: [bucket].json)')
     .parse(process.argv);
 if(!program.args.length) {
     program.help();
@@ -15,7 +15,7 @@ if(!program.args.length) {
 var bucket = program.args;
 program.host = program.host || 'localhost';
 program.port = program.port || '8098';
-program.file = program.file || bucket+'.txt';
+program.file = program.file || bucket+'.json';
 var count = 0;
 var openWrites = 0;
 var db = require("riak-js").getClient({host: program.host, port: program.port});
@@ -24,6 +24,7 @@ if (fs.existsSync(program.file)) {
   throw new Error('the output file already exists');
 }
 console.log('fetching bucket '+bucket+' from '+program.host+':'+program.port);
+fs.appendFile(program.file, '[');
 db.keys(bucket,{keys:'stream'}, function (err) {
   if (err) {
     console.log('failed to fetch keys');
@@ -36,6 +37,7 @@ function end() {
     setTimeout(end, 1000);
     return;
   }
+  fs.appendFile(program.file, ']');
   if (count<=0) {
     console.log('nothing exported');
   } else {
@@ -55,14 +57,32 @@ function handleKey(keys) {
   }
 }
 
+var first = true;
 function processKey(key) {
   db.get(bucket,key, function(err, obj, meta) {
-    var out = '====== Key: '+key+' ======\n';
-    out += JSON.stringify(obj, null, '\t')+'\n';
-    fs.appendFile(program.file, out);
+    var out = {key: key};
+    out.indexes = extractIndexes(meta);
+    out.data = obj;
+    if (!first) {
+      fs.appendFile(program.file, ',');
+    }
+    fs.appendFile(program.file, JSON.stringify(out,null,'\t'));
+    first=false;
     process.stdout.write(".");
     openWrites--;
   });
+}
+
+function extractIndexes(meta) {
+  var indexes = {};
+  var regex = /^x-riak-index-(.*)$/;
+  for (var key in meta.headers) {
+    var matches = key.match(regex);
+    if (matches) {
+      indexes[matches[1]] = meta.headers[key];
+    }
+  }
+  return indexes;
 }
 
 
